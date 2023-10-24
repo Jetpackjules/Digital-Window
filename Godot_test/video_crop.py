b8b02e513bb2f7db8859345596466eec4cd11b22
Jetpackjules <jetpackjules@gmail.com>
2023-10-24 12:03:19 -0700

    kl,m

--- Godot_test/video_crop.py
+++ Godot_test/video_crop.py
@@ -1,6 +1,31 @@
 import cv2
 import numpy as np
-from tqdm import tqdm
+import tqdm
+
+def resize_to_aspect_ratio(image, target_ratio=16/9, target_width=1280):
+    """Resize the image to maintain the target aspect ratio."""
+    h, w, _ = image.shape
+    current_ratio = w / h
+
+    if current_ratio > target_ratio:
+        # Current aspect ratio is greater than target, adjust height
+        new_height = int(w / target_ratio)
+        padding_top = (new_height - h) // 2
+        padding_bottom = new_height - h - padding_top
+        image = cv2.copyMakeBorder(image, padding_top, padding_bottom, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    elif current_ratio < target_ratio:
+        # Current aspect ratio is less than target, adjust width
+        new_width = int(h * target_ratio)
+        padding_left = (new_width - w) // 2
+        padding_right = new_width - w - padding_left
+        image = cv2.copyMakeBorder(image, 0, 0, padding_left, padding_right, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+    # Resize the image to the target width while maintaining the aspect ratio
+    new_height = int(target_width / target_ratio)
+    image = cv2.resize(image, (target_width, new_height))
+
+    return image
+
 
 def order_points(pts):
     x_sorted = pts[np.argsort(pts[:, 0]), :]
@@ -15,66 +40,71 @@ def order_points(pts):
 def transform_perspective(image, pts):
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
-    width_a = np.linalg.norm(br - bl)
-    width_b = np.linalg.norm(tr - tl)
-    height_a = np.linalg.norm(tr - tl)
-    height_b = np.linalg.norm(br - bl)
-    max_width = max(int(width_a), int(width_b))
-    max_height = max(int(height_a), int(height_b))
-    aspect_ratio = 16 / 9
-    if max_width / max_height > aspect_ratio:
-        max_height = int(max_width / aspect_ratio)
-    else:
-        max_width = int(max_height * aspect_ratio)
+    width = max(int(np.linalg.norm(br - bl)), int(np.linalg.norm(tr - tl)))
+    height = max(int(np.linalg.norm(tr - tl)), int(np.linalg.norm(br - bl)))
     dst = np.array([
         [0, 0],
-        [max_width - 1, 0],
-        [max_width - 1, max_height - 1],
-        [0, max_height - 1]
+        [width - 1, 0],
+        [width - 1, height - 1],
+        [0, height - 1]
     ], dtype="float32")
     M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (max_width, max_height))
+    warped = cv2.warpPerspective(image, M, (width, height))
     return warped
 
 def is_black_pixel(image, point):
     rgb = image[int(point[1]), int(point[0])]
-    return all(rgb < [25, 25, 25])
+    return all(rgb < [25, 25, 25])\
 
-# Open the video file
+
+# Load the video
 cap = cv2.VideoCapture('Godot_test\input_video.mp4')
 fps = int(cap.get(cv2.CAP_PROP_FPS))
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Use 'mp4v' codec for MP4
-out = cv2.VideoWriter('Godot_test\output_video.mp4', fourcc, fps, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
-# Get the total number of frames for the progress bar
-total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+# Initialize the VideoWriter with a placeholder size
+out = None
 
 # Process each frame
-for _ in tqdm(range(total_frames), desc="Processing video"):
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+for _ in tqdm.tqdm(range(frame_count), desc="Processing video"):
     ret, frame = cap.read()
     if not ret:
-        print("Failed to read a frame!")
-        continue
+        print("Failed to read frame.")
+        break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 15, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    possible_rectangles = []
+    max_area = 0
+    best_approx = None
     for contour in contours:
         approx = cv2.approxPolyDP(contour, 0.04 * cv2.arcLength(contour, True), True)
-        if len(approx) == 4 and all([is_black_pixel(frame, pt[0]) for pt in approx]):
-            possible_rectangles.append(contour)
+        if len(approx) == 4 and cv2.contourArea(approx) > max_area:
+            all_corners_black = all([is_black_pixel(frame, pt[0]) for pt in approx])
+            if all_corners_black:
+                max_area = cv2.contourArea(approx)
+                best_approx = approx
 
-    if possible_rectangles:
-        c = max(possible_rectangles, key=cv2.contourArea)
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-        warped = transform_perspective(frame, approx.reshape(4, 2))
+    if best_approx is not None:
+        warped = transform_perspective(frame, best_approx.reshape(4, 2))
+        warped = resize_to_aspect_ratio(warped)
+        cv2.imshow('Cropped Frame', warped)
+        
+        # Initialize the VideoWriter with the correct size once we have the first cropped frame
+        if out is None:
+            h, w, _ = warped.shape
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            out = cv2.VideoWriter('Godot_test\output_video.mp4', fourcc, fps, (w, h))
+        
         out.write(warped)
-    else:
-        print("No rectangle found in a frame!")
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+
+
+# Release everything
 cap.release()
-out.release()
-print("Video processing complete!")
+if out:
+    out.release()
+cv2.destroyAllWindows()
